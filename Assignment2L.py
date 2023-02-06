b8d4984242e98122b2f693a2fa4ba6c423b57716
RomanDattilo <114018508+RomanDattilo@users.noreply.github.com>
2023-02-06 12:53:24 -0500

    Add files via upload

--- Assignment2L.py
+++ Assignment2L.py
@@ -4,39 +4,43 @@
 
 #restaurant
 
+while on_go == 'yes':
+
+    vegetarian=False
+    vegan=False
+    gluten_free=False
+
+    answer1=(input("Are any of your friends vegatarian? "))
+    if answer1=='yes':
+        vegetarian=True
+
+    answer2=(input("Are any of your friends vegan? "))
+    if answer2=='yes':
+        vegan=True
+
+    answer3=(input("Are any of your friends gluten-free? "))
+    if answer3=='yes':
+        gluten_free=True
 
 
-vegetarian=False
-vegan=False
-gluten_free=False
+    print("Here are your restaurant choices: ")
 
-answer1=(input("Are any of your friends vegatarian? "))
-if answer1=='yes':
-    vegetarian=True
+    if vegetarian==False and vegan==False and gluten_free==False:
+        print("Joe's Gourmet Burgers'")
 
-answer2=(input("Are any of your friends vegan? "))
-if answer2=='yes':
-    vegan=True
+    if vegetarian==True and vegan==False and gluten_free==False:
+        print("Mama's Fine Italian'")
 
-answer3=(input("Are any of your friends gluten-free? "))
-if answer3=='yes':
-    gluten_free=True
+    if vegetarian==True and vegan==False and gluten_free==True:
+        print("Main Street Pizza")
+    else:
+        print("Corner Cafe")
+        print("Chef's Kitchen")
+        
+    if on_go== input("Do you want to keep going? "):
 
 
-print("Here are your restaurant choices: ")
-
-if vegetarian==False and vegan==False and gluten_free==False:
-    print("Joe's Gourmet Burgers'")
-
-if vegetarian==True and vegan==False and gluten_free==False:
-    print("Mama's Fine Italian'")
-
-if vegetarian==True and vegan==False and gluten_free==True:
-    print("Main Street Pizza")
-else:
-    print("Corner Cafe")
-    print("Chef's Kitchen")
-
+    
 
 
 
