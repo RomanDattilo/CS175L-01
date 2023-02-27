f8b5e527e1511a08aa5cc2c0b97566ffa0db8beb
RomanDattilo <114018508+RomanDattilo@users.noreply.github.com>
2023-02-27 12:24:58 -0500

    Add files via upload
    I forgot to repeat it this should fix

--- Assignment4L.py
+++ Assignment4L.py
@@ -19,8 +19,52 @@ def calc_average():
     print('Score 5: ', f'{Score5:>8}', f'{determine_grade(Score5):>17}')
     print('Average Score: ', avg)
 
-    
-
+    print()
+    cont=input('Enter yes if you would like to do anther calculation: ')
+    print()
+    if cont=='yes':
+        Score1=int(input('Enter score 1: '))
+        Score2=int(input('Enter score 2: '))
+        Score3=int(input('Enter score 3: '))
+        Score4=int(input('Enter score 4: '))
+        Score5=int(input('Enter score 5: '))
+        avg=Score1+Score2+Score3+Score4+Score5/5
+        print()
+        print("Score		Numeric Grade	   Letter Grade")
+        print("----------------------------------------------------")
+        print('Score 1: ', f'{Score1:>8}', f'{determine_grade(Score1):>17}')
+        print('Score 2: ', f'{Score2:>8}', f'{determine_grade(Score2):>17}')
+        print('Score 3: ', f'{Score3:>8}', f'{determine_grade(Score3):>17}')
+        print('Score 4: ', f'{Score4:>8}', f'{determine_grade(Score4):>17}')
+        print('Score 5: ', f'{Score5:>8}', f'{determine_grade(Score5):>17}')
+        print('Average Score: ', avg)
+           
+            
+    else:
+        print('Goodbye!')
+    print()
+    cont=input('Enter yes if you would like to do anther calculation: ')
+    print()
+    if cont=='yes':
+        Score1=int(input('Enter score 1: '))
+        Score2=int(input('Enter score 2: '))
+        Score3=int(input('Enter score 3: '))
+        Score4=int(input('Enter score 4: '))
+        Score5=int(input('Enter score 5: '))
+        avg=Score1+Score2+Score3+Score4+Score5/5
+        print()
+        print("Score		Numeric Grade	   Letter Grade")
+        print("----------------------------------------------------")
+        print('Score 1: ', f'{Score1:>8}', f'{determine_grade(Score1):>17}')
+        print('Score 2: ', f'{Score2:>8}', f'{determine_grade(Score2):>17}')
+        print('Score 3: ', f'{Score3:>8}', f'{determine_grade(Score3):>17}')
+        print('Score 4: ', f'{Score4:>8}', f'{determine_grade(Score4):>17}')
+        print('Score 5: ', f'{Score5:>8}', f'{determine_grade(Score5):>17}')
+        print('Average Score: ', avg)
+           
+            
+    else:
+        print('Goodbye!')
 
 
     
